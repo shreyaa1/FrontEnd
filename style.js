f052b04178202ba8a25cd5195d4a04f7ab58abe9
shreya shukla <shuklashreya801@gmail.com>
2018-01-26 01:02:28 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,13 @@
+	function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("menu").style.marginRight = "0px";
+          document.getElementById("menu").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("menu").style.marginRight= "0";
+          document.getElementById("menu").style.display = "block";
+}
+
+
