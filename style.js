3d6626dcf81c0fc58448f0bc120c045c51d13af0
shreya shukla <shuklashreya801@gmail.com>
2018-01-26 14:02:28 +0530

    responsive

--- style.js
+++ style.js
@@ -11,3 +11,24 @@ function closeNav() {
 }
 
 
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("model-button");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+	window.onkeydown = function( event ) {
+    if ( event.keyCode == 27 ) {
+        modal.style.display = "none";
+    }
+};
+
